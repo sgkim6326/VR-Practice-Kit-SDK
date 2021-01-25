4309511b803863c8cea6b6a4677f8b9ba03cabd8
김수영 <dirtyfly58@gmail.com>
2021-01-26 05:18:19 +0900

    VR Kit SDK(플러그인 제외)

---
+++ Assets/VRKit/Scripts/Editor/VRKitEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace VRKit
+{
+    [CustomEditor(typeof(VRKitCore)), CanEditMultipleObjects]
+    public class VRKitEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            VRKitCore core = (VRKitCore)target;
+            core.GetComponent<AudioSource>().hideFlags = HideFlags.HideInInspector;
+            core.ConnectionFailedSound.hideFlags = HideFlags.HideInInspector;
+            core.ConnectionSucessSound.hideFlags = HideFlags.HideInInspector;
+            core.DisconnectedSound.hideFlags = HideFlags.HideInInspector;
+            core.StartConnectionSound.hideFlags = HideFlags.HideInInspector;
+        }
+    }
+
+}
