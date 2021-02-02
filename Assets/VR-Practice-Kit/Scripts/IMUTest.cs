03cea429abe71f150fe03314f2482cb8eb3eb586
김수영 <dirtyfly58@gmail.com>
2021-02-02 17:20:56 +0900

    회전 기능 업데이트

---
+++ Assets/VR-Practice-Kit/Scripts/IMUTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRKit;
+
+public class IMUTest : MonoBehaviour
+{
+    void Update()
+    {
+        transform.localRotation = VRKitCore.GetRotation();
+    }
+}
+
+
+
---
+++ Assets/VR-Practice-Kit/Scripts/JoystickTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRKit;
+
+public class JoystickTest : MonoBehaviour
+{
+    void Update()
+    {
+        var input_vertical = VRKitCore.GetAxis("Vertical");
+        var input_horizontal = VRKitCore.GetAxis("Horizontal");
+
+        var input_jump = VRKitCore.GetKeyDown();
+
+        input_vertical = input_vertical * Time.deltaTime;
+        input_horizontal = input_horizontal * Time.deltaTime * 100;
+
+        transform.Translate(transform.forward * input_vertical);
+        transform.Rotate(transform.up * input_horizontal);
+
+        if (input_jump)
+        {
+            transform.Translate(transform.up);
+        }
+    }
+}
+
+
+
