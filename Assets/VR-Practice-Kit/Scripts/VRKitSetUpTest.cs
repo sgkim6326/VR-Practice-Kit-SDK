ce9ded050bff0e326cd9cff46fbe18a2cafea883
sgkim58 <dirtyfly58@gmail.com>
2021-04-12 17:56:29 +0900

    VPT Vendor ID 설정 씬 추가

---
+++ Assets/VR-Practice-Kit/Scripts/VRKitSetUpTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class VRKitSetUpTest : MonoBehaviour
+{
+    public InputField EnterVendorID;
+    public Button LoadJoystickTest, LoadImuTest;
+    public VRKit.VRKitCore VRKitManager;
+    void Awake() => XRSettings.enabled = false;
+    void Start()
+    {
+        LoadJoystickTest.onClick.AddListener(() =>
+        {
+            VRKitManager.deviceName = EnterVendorID.text;
+            VRKitManager.gameObject.SetActive(true);
+            XRSettings.enabled = true;
+            SceneManager.LoadScene("Character-Control-Example");
+        });
+        LoadImuTest.onClick.AddListener(() =>
+        {
+            VRKitManager.deviceName = EnterVendorID.text;
+            VRKitManager.gameObject.SetActive(true);
+            XRSettings.enabled = true;
+            SceneManager.LoadScene("UI-Control-Example");
+        });
+    }
+}
